6d4228d73a12efbb2843de8613fb5451f6008219
Lee <lhc1@rice.edu>
2018-03-21 23:51:19 -0500

    commit

--- Configurations.py
+++ Configurations.py
@@ -1,3 +1,7 @@
 inputFolder = "InputFiles"
 generateFolder = "Generated"
 resultFolder = "Results"
+
+mosaicFinderDir = "./MosaicFinder/mosaicFinder"
+filteredDir = "Generated/filteredAllToAll"
+cPickleDir = "Generated/borderDict"
--- run.py
+++ run.py
@@ -1,8 +1,45 @@
+from src import util
 import Configurations as conf
+import subprocess
+import os
+
+
+def filterInput():
+    util.generateDirectories(conf.filteredDir)
+    filenames = os.listdir(conf.inputFolder)
+    for filename in filenames:
+        with open(os.path.join(conf.filteredDir, filename), "w") as wf, \
+                open(os.path.join(conf.inputFolder, filename), "r") as rf:
+            for line in rf:
+                arr = line.split("\t")
+                if arr[0] != arr[1]:
+                    wf.write(line)
+
+
+def convertResultToCPickleDict():
+    util.generateDirectories(conf.cPickleDir)
+    filenames = os.listdir(conf.resultFolder)
+    for filename in filenames:
+        with open(os.path.join(conf.resultFolder, filename), "r") as f:
+            for line in f:
+
 
 
 def main():
-    pass
+    filterInput()
+
+    filenames = os.listdir(conf.filteredDir)
+
+    util.generateDirectories(conf.resultFolder)
+
+    for filename in filenames:
+        fileDir = os.path.join(conf.filteredDir, filename)
+
+        cmd = [conf.mosaicFinderDir, "-e", "1e-05", "-p", "30", "-i", fileDir, "-o", os.path.join(conf.resultFolder, filename)]
+
+        proc = subprocess.Popen(cmd)
+
+    return 0
 
 
 if __name__ == '__main__':
